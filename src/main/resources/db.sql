0def3ebf3e034124237cabd1598fb489f4d74a44
euhuduwu <csluo@ctrip.com>
2018-12-10 20:29:16 +0800

    身份权限相关db

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -10,3 +10,40 @@ CREATE TABLE `user_info` (
   PRIMARY KEY (`user_info_id`),
   KEY `ix_datachange_lasttime` (`datachange_lasttime`)
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 DEFAULT CHARSET = utf8 COMMENT '用户信息';
+
+CREATE TABLE `sys_permission` (
+  `sys_permission_id` bigint(20) NOT NULL,
+  `parent_id` bigint(20) DEFAULT NULL,
+  `res_name` varchar(50) DEFAULT NULL,
+  `res_type` varchar(10) DEFAULT NULL,
+  `permission` varchar(20) DEFAULT NULL,
+  `url` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`sys_permission_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `sys_role` (
+  `role_id` bigint(20) NOT NULL,
+  `role_name` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `sys_role_permission` (
+  `role_id` bigint(20) NOT NULL,
+  `permission_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`role_id`,`permission_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `sys_user` (
+  `user_id` bigint(20) NOT NULL,
+  `user_name` varchar(50) DEFAULT NULL,
+  `full_name` varchar(20) DEFAULT NULL,
+  `password` varchar(32) DEFAULT NULL,
+  `salt` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `sys_user_role` (
+  `user_id` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
