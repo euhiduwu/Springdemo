b44658725117c82b4dc1006d90ccd43a54f29ba9
euhuduwu <csluo@ctrip.com>
2018-12-10 17:37:00 +0800

    设置ehcache缓存,redis需要服务器

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @SpringBootApplication
 @MapperScan("com.example.demo.mapper")
+@EnableCaching // cache
 public class Application extends WebMvcConfigurationSupport {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/demo/service/UserInfoService.java
+++ src/main/java/com/example/demo/service/UserInfoService.java
@@ -5,6 +5,7 @@ import com.example.demo.entity.UserInfoExample;
 import com.example.demo.mapper.UserInfoMapper;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,7 +18,10 @@ public class UserInfoService {
         this.userInfoMapper = userInfoMapper;
     }
 
+//    @Cacheable(cacheNames = "userInfo",key = "#page")
+//    缓存 name
     public PageInfo<UserInfo> getUserInfo(Integer page, Integer number) {
+        System.out.printf("没有缓存，开始访问数据库...");
         if (page != null && number != null) {
             PageHelper.startPage(page, number);
         }
