08a31e863d0d712588f1ac8d91ae629ffc93aac7
euhuduwu <csluo@ctrip.com>
2018-12-11 16:40:53 +0800

    解决静态资源不能访问

---
+++ src/main/java/com/example/demo/config/InterceptorConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig extends WebMvcConfigurationSupport {
+
+//    @Override
+//    protected void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new UserInterceptor()).addPathPatterns("/user/**");
+//        super.addInterceptors(registry);
+//    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        super.addResourceHandlers(registry);
+    }
+
+}
